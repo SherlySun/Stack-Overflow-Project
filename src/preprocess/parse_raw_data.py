09bdca8a2a81f10dfc9cf5af18f65a9ce8d3d1e3
Jyun-Yu Jiang <jyunyu@qilin.cs.ucla.edu>
2017-05-25 01:04:03 -0700

    Parse the remaining xml files for preprocessing.

--- src/preprocess/parse_raw_data.py
+++ src/preprocess/parse_raw_data.py
@@ -7,6 +7,8 @@ from lxml import etree
 from html2text import html2text
 from html.parser import HTMLParser
 from bs4 import BeautifulSoup
+import nltk
+
 
 # lxml.html.document_fromstring(html_string)
 
@@ -15,6 +17,22 @@ try:
 except:
     import json
 
+
+def clean_html(x):
+    return BeautifulSoup(x, 'lxml').get_text()
+
+
+def preprocess_general(PATH_RAW, PATH_DATA, raw_file, output_file):
+    html_parser = HTMLParser()
+    parser = etree.iterparse(PATH_RAW + raw_file, events=('end',), tag='row')
+    with open(PATH_DATA + output_file, 'w') as fout:
+        for event, elem in parser:
+            attr = dict(elem.attrib)
+            print(json.dumps(attr), file=fout)
+            elem.clear()
+            while elem.getprevious() is not None:
+                del elem.getparent()[0]
+
 def preprocess_posts(PATH_RAW, PATH_DATA):
     html_parser = HTMLParser()
     parser = etree.iterparse(PATH_RAW + 'Posts.xml', events=('end',), tag='row')
@@ -22,16 +40,31 @@ def preprocess_posts(PATH_RAW, PATH_DATA):
          open(PATH_DATA + 'posts_answer.json', 'w') as fout_a:
         for event, elem in parser:
             attr = dict(elem.attrib)
-            #attr['Body'] = html_parser.unescape(attr['Body'])
-            attr['Body'] = BeautifulSoup(attr['Body'], 'lxml').get_text()
+            attr['Body'] = clean_html(attr['Body'])
             print(json.dumps(attr), file=fout_q if attr['PostTypeId'] == '1' else fout_a)
             elem.clear()
             while elem.getprevious() is not None:
                 del elem.getparent()[0]
 
+def preprocess_comments(PATH_RAW, PATH_DATA):
+    html_parser = HTMLParser()
+    parser = etree.iterparse(PATH_RAW + 'Comments.xml', events=('end',), tag='row')
+    with open(PATH_DATA + 'comments.json', 'w') as fout:
+        for event, elem in parser:
+            attr = dict(elem.attrib)
+            attr['Text'] = html_parser.unescape(attr['Text'])
+            print(json.dumps(attr), file=fout)
+            elem.clear()
+            while elem.getprevious() is not None:
+                del elem.getparent()[0]
+
+
+
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 1 + 1:
-        print('--usage %s [name of dataset]' % sys.argv[0], file=sys.stderr) 
+        print('--usage %s name_of_the_dataset' % sys.argv[0], file=sys.stderr) 
         sys.exit(0)
 
     # load 
@@ -48,3 +81,10 @@ if __name__ == '__main__':
 
     # start preprocessing 
     preprocess_posts(PATH_RAW, PATH_DATA)
+    preprocess_comments(PATH_RAW, PATH_DATA)
+    preprocess_general(PATH_RAW, PATH_DATA, 'Badges.xml', 'badges.json')
+    preprocess_general(PATH_RAW, PATH_DATA, 'PostLinks.xml', 'postlinks.json')
+    preprocess_general(PATH_RAW, PATH_DATA, 'Tags.xml', 'tags.json')
+    preprocess_general(PATH_RAW, PATH_DATA, 'Users.xml', 'users.json')
+    preprocess_general(PATH_RAW, PATH_DATA, 'Votes.xml', 'votes.json')
+    preprocess_general(PATH_RAW, PATH_DATA, 'PostHistory.xml', 'posthistory.json')
