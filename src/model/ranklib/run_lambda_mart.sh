3bb53909b0c67881c5a8bdc73118da5ce2ac75e2
hallogameboy <hallogameboy@gmail.com>
2017-05-31 23:21:33 -0700

    Fix the argument issue

--- src/model/ranklib/run_lambda_mart.sh
+++ src/model/ranklib/run_lambda_mart.sh
@@ -4,7 +4,11 @@ if [ $# == 0 ]; then
     echo "usage: $0 dataset"
     exit
 else
-    java -jar RankLib.jar -train ../../../features/mathoverflow/train.libsvm -test ../../../features/mathoverflow/test.libsvm -ranker 6 -metric2t MAP -tvs 0.8 -shrinkage 0.1
+    java -jar RankLib.jar \
+        -train ../../../features/$1/train.libsvm \
+        -test ../../../features/$1/test.libsvm \
+        -save ../../../features/$1/model.lambdamart
+        -ranker 6 -metric2t MAP -tvs 0.8 -shrinkage 0.1 
 fi
 
    
