a94b205b906229b5fa3b29857f028fb0748840de
hallogameboy <hallogameboy@gmail.com>
2017-06-03 18:19:00 -0700

    Add the code for node2vec

--- src/feature_extraction/batch_extract.sh
+++ src/feature_extraction/batch_extract.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-./comment_cnt.py $1
-./user_age.py $1
-./user_badge.py $1
-./user_reputation.py $1
-./user_user_interactions.py $1
-./user_views.py $1 
-./user_votes.py $1
+#./comment_cnt.py $1
+#./user_age.py $1
+#./user_badge.py $1
+#./user_reputation.py $1
+#./user_user_interactions.py $1
+#./user_views.py $1 
+#./user_votes.py $1
 
 echo "Constructing user lists under same questions"
 ./sn_user_common_question.py $1
